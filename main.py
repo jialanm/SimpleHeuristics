5e76be564986ae57508f253c1002cb5eaefd32c7
jialanm <majialan@broadinstitute.org>
2024-05-20 19:43:23 -0400

    add coverage for comparison between samples and GTEx

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """
 Created a set of heuristics to recover causal splice junctions in the muscle truth set.
 """
+import os
 
 import numpy as np
 import pandas as pd
 import hail as hl
 import gzip
+import pyBigWig
 
 CHROM_SET = set([f"chr{i}" for i in range(1, 23)] + ["chrX", "chrY"])
 DESCRIPTION_COLS = ["motif", "uniquely_mapped", "multi_mapped",
@@ -56,7 +58,8 @@ TRUTH_SET = {"41M_MW_M1": "chr12:21440448-21449562",
              "205E_BD_M1": "chr21:46126237-46131953"}
 
 GTEx = "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.junctions.bed.gz"
-GTEx_normalized = "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.normalized.junctions.bed.gz"
+GTEx_NORMALIZED_BED = "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.normalized.junctions.bed.gz"
+GTEX_NORMALIZED_BIGWIG = "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.normalized.bigWig"
 
 BATCH_2023 = {"RGP_2058_3_M1", "BON_B22_12_1_R1"}
 BATCH_2022 = {"BON_B16-59_1"}
@@ -133,10 +136,22 @@ def get_sj_only_in_first_dat(dat1, dat2):
     return merged_dat
 
 
-def get_sj_different_between_dats(dat1, dat2, lr_threshold):
+def get_sj_different_between_dats(dat1, dat2, lr_sj_threshold, lr_coverage_threshold):
+    pd.set_option('display.max_columns', None)
     merged_dat = pd.merge(dat1, dat2, how='inner', on=["chr", "start", "end"],
                           suffixes=('', '_y'))
-    merged_dat["lr"] = abs(np.log(merged_dat["normalized"] / merged_dat["normalized_y"]))
+    pseudocount = 1e-12
+    sj_ratio = (merged_dat["normalized_by_sj"] + pseudocount) / (merged_dat[
+                                                                     "normalized_by_sj_y"] + pseudocount)
+
+    coverage_ratio = (merged_dat["normalized_by_coverage"] + pseudocount) / (merged_dat[
+                                                                                 "normalized_by_coverage_y"] + pseudocount)
+    # for i in range(coverage_ratio.shape[0]):
+    #     if coverage_ratio[i] < 0:
+    #         print(merged_dat.iloc[i, :])
+
+    merged_dat["lr_sj"] = abs(np.log2(sj_ratio))
+    merged_dat["lr_coverage"] = abs(np.log2(coverage_ratio))
     # # Test
     # pd.set_option('display.max_columns', None)
     # cur_sample = "UC84-1RNA"
@@ -144,26 +159,42 @@ def get_sj_different_between_dats(dat1, dat2, lr_threshold):
     # is_present_causal_sj(merged_dat, causal_sj)
     # #
 
-    merged_dat = merged_dat[merged_dat["lr"] > lr_threshold]
+    pd.set_option('display.max_columns', None)
+    # print(merged_dat)
+    # is_present_causal_sj(dat1, TRUTH_SET["B14-78-1-U"])
+    # print("-----------------------------------")
+    # is_present_causal_sj(dat2, TRUTH_SET["B14-78-1-U"])
+    # print("-----------------------------------")
+    # is_present_causal_sj(merged_dat, TRUTH_SET["CLA_180CJ_DP_2"])
+    # print("-----------------------------------")
+
+    # merged_dat = merged_dat[(merged_dat["lr_sj"] > lr_sj_threshold)]
+    merged_dat = merged_dat[(merged_dat["lr_sj"] > lr_sj_threshold) | (
+            merged_dat["lr_coverage"] > lr_coverage_threshold)]
+    # merged_dat = merged_dat[merged_dat["lr_coverage"] > lr_coverage_threshold]
     merged_dat = merged_dat.drop(
         columns=[col for col in merged_dat.columns if col.endswith("_y")])
-    merged_dat = merged_dat.drop(columns=["lr"])
+    # is_present_causal_sj(merged_dat, TRUTH_SET["CLA_180CJ_DP_2"])
+    merged_dat = merged_dat.drop(columns=["lr_sj", "lr_coverage"])
 
     return merged_dat
 
 
-def compare_with_gtex(gtex_normalized_dat, dat, lr_threshold):
+def compare_with_gtex(gtex_normalized_dat, dat, lr_sj_threshold, lr_coverage_threshold):
     # in_rd_not_in_gtex_dat = get_sj_only_in_first_dat(dat, gtex_dat)
     in_rd_not_in_gtex_normalized_dat = get_sj_only_in_first_dat(dat,
                                                                 gtex_normalized_dat)
     # in_gtex_not_in_rd_dat = get_sj_only_in_first_dat(gtex_dat, dat)
-    # in_gtex_normalized_not_in_rd_dat = get_sj_only_in_first_dat(
-    #     gtex_normalized_dat, dat)
+    in_gtex_normalized_not_in_rd_dat = get_sj_only_in_first_dat(gtex_normalized_dat,
+                                                                dat)
     different_between_rd_and_gtex = get_sj_different_between_dats(dat,
                                                                   gtex_normalized_dat,
-                                                                  lr_threshold)
+                                                                  lr_sj_threshold=2,
+                                                                  lr_coverage_threshold=2)
 
-    res = pd.concat([in_rd_not_in_gtex_normalized_dat, different_between_rd_and_gtex])
+    res = pd.concat([in_rd_not_in_gtex_normalized_dat,
+                     # in_gtex_normalized_not_in_rd_dat,
+                     different_between_rd_and_gtex])
     return res
     # pass
     # diff_dat = pd.concat([in_rd_not_in_gtex_normalized_dat,
@@ -192,27 +223,94 @@ def read_junctions_bed(junctions_bed_path, use_cols=None):
     return bed_dat
 
 
-def normalize_reads_by_gene(dat):
+def read_coverage_bigwig(bigwig_path):
+    local_path = os.path.basename(bigwig_path)
+    if not os.path.exists(local_path):
+        os.system(f"gcloud storage cp {bigwig_path} {local_path}")
+
+    bw = pyBigWig.open(local_path)
+
+    return bw
+
+
+def merge_overlapping_segments(dat):  # TODO: check the algorithm
+    dat = dat.sort_values(by=["start", "end"])
+    segments = []
+    last_start = None
+    last_end = None
+    chr = dat["chr"].iloc[0]
+    for index, row in dat.iterrows():
+        start = row["start"]
+        end = row["end"]
+
+        if last_start is None:
+            last_start = start
+            last_end = end
+        else:
+            if start <= last_end:
+                last_end = max(last_end, end)
+            else:
+                segments.append((chr, last_start, last_end))
+                last_start = start
+                last_end = end
+    segments.append((chr, last_start, last_end))
+    return segments
+
+
+def normalize_reads_by_gene(dat, coverage_bw):
     """
     Normalize the number of reads by dividing the reads of a splice junction by the
-    total number of reads of all splice junctions within the same gene.
-    :param dat:
-    :return:
+    total number of reads of all splice junctions within the gene. Also, normalize the
+    reads by the coverage of the gene.
     """
-    # Get the total number of reads for each gene.
+
+    # Normalize by total reads of splice junctions within the gene.
     gene_total_sj_reads = dat.groupby("gene")["uniquely_mapped"].sum().reset_index()
     gene_total_sj_reads.columns = ["gene", "total_reads"]
     gene_total_sj_reads_dict = {}
     for i in range(gene_total_sj_reads.shape[0]):
-        gene_total_sj_reads_dict[gene_total_sj_reads.iloc[i, 0]] = gene_total_sj_reads.iloc[i, 1]
+        gene_total_sj_reads_dict[gene_total_sj_reads.iloc[i, 0]] = \
+            gene_total_sj_reads.iloc[i, 1]
 
-    print(gene_total_sj_reads_dict)
-    dat["normalized"] = dat["uniquely_mapped"] / dat["gene"].map(
+    # print(gene_total_sj_reads_dict)
+    dat["normalized_by_sj"] = dat["uniquely_mapped"] / dat["gene"].map(
         gene_total_sj_reads_dict)
+
+    # Normalize by the coverage of the gene.
+    sum_gene_coverage_dict = {}
+    mean_gene_coverage_dict = {}
+    for gene in DISEASE_GENES:
+        cur_dat = dat[dat["gene"] == gene]
+        gene_coverage = 0
+        sj_segments = merge_overlapping_segments(cur_dat)
+        for segment in sj_segments:
+            gene_coverage += np.nansum(coverage_bw.values(segment[0], segment[1],
+                                                          segment[2]))
+        # Remove 0 values because they are not informative and will skew the mean.
+        # gene_coverage = [i for i in gene_coverage if i != 0]
+        sum_gene_coverage_dict[gene] = gene_coverage
+        num_of_bases = 0
+        for segment in sj_segments:
+            num_of_bases += segment[2] - segment[1]
+
+        mean_gene_coverage_dict[gene] = gene_coverage / num_of_bases
+
+    # print(mean_gene_coverage_dict)
+    dat["sj_coverage"] = dat.apply(lambda row: np.sum(coverage_bw.values(row["chr"],
+                                                                         row["start"],
+                                                                         row["end"])),
+                                   axis=1)
+
+    # pd.set_option('display.max_columns', None)
+    # is_present_causal_sj(dat, TRUTH_SET["CLA_180CJ_DP_2"])
+    # print("-----------------------------------")
+    dat["normalized_by_coverage"] = dat["sj_coverage"] / dat["gene"].map(
+        sum_gene_coverage_dict)
+
     return dat
 
 
-def apply_filters(bed_dat, sample_id, gtex_normalized_dat):
+def apply_filters(bed_dat, sample_id, gtex_normalized_dat, bigwig_path):
     print("-----------------------------------")
     print(bed_dat.shape)
     # Decision rules to filter out splice junctions.
@@ -228,15 +326,29 @@ def apply_filters(bed_dat, sample_id, gtex_normalized_dat):
     # casual_sj = TRUTH_SET[sample_id]
     # is_present = is_present_causal_sj(filtered_bed_dat, casual_sj)
 
-    # 3. Compare with GTEx data.
-    normalized_filtered_bed_dat = normalize_reads_by_gene(filtered_bed_dat)
-    normalized_gtex_normalized_dat = normalize_reads_by_gene(filtered_gtex_normalized_dat)
+    # 3. Compare reads and coverage with GTEx data.
+    # Get the coverage file.
+    bw = read_coverage_bigwig(bigwig_path)
+    gtex_bw = read_coverage_bigwig(GTEX_NORMALIZED_BIGWIG)
+
+    normalized_filtered_bed_dat = normalize_reads_by_gene(filtered_bed_dat, bw)
+    normalized_gtex_normalized_dat = normalize_reads_by_gene(
+        filtered_gtex_normalized_dat, gtex_bw)
     filtered_bed_dat = compare_with_gtex(normalized_gtex_normalized_dat,
                                          normalized_filtered_bed_dat,
-                                         lr_threshold=2)
+                                         lr_sj_threshold=2,
+                                         lr_coverage_threshold=2)
     print(filtered_bed_dat.shape)
+    grouped_gene = filtered_bed_dat.groupby("gene").size()
+    print(grouped_gene)
 
-    # 4. Filter by maximum spliced alignment overhang.
+    # 4. Compare coverage with GTEx data.
+
+    # Close the BigWig file
+    bw.close()
+    # os.system(f"rm *.bigWig")
+
+    # 5. Filter by maximum spliced alignment overhang.
     # filtered_bed_dat = filtered_bed_dat[filtered_bed_dat[
     #                                         "maximum_spliced_alignment_overhang"] > 2]
     # print(filtered_bed_dat.shape)
@@ -257,11 +369,11 @@ def main():
     # gtex_dat = read_junctions_bed(GTEx, USE_COLS)
     # gtex_dat = filter_by_uniquely_mapped_reads(gtex_dat, 1)
 
-    gtex_normalized_dat = read_junctions_bed(GTEx_normalized, USE_COLS)
+    gtex_normalized_dat = read_junctions_bed(GTEx_NORMALIZED_BED, USE_COLS)
     gtex_normalized_dat = filter_by_uniquely_mapped_reads(gtex_normalized_dat, 1)
 
     for cur_sample in TRUTH_SET:
-        # if cur_sample != "UC84-1RNA":
+        # if cur_sample != "CLA_180CJ_DP_2":
         #     continue
         if cur_sample in BATCH_2023:
             junctions_bed_path = f"gs://tgg-rnaseq/batch_2023_01/junctions_bed_for_igv_js/{cur_sample}.junctions.bed.gz"
@@ -270,9 +382,12 @@ def main():
         else:
             junctions_bed_path = f"gs://tgg-rnaseq/batch_0/junctions_bed_for_igv_js/{cur_sample}.junctions.bed.gz"
 
+        bigwig_path = f"gs://tgg-rnaseq/batch_0/bigWig/{cur_sample}.bigWig"
+
         bed_dat = read_junctions_bed(junctions_bed_path, USE_COLS)
         filtered_bed_dat = apply_filters(bed_dat, cur_sample,
-                                         gtex_normalized_dat)
+                                         gtex_normalized_dat,
+                                         bigwig_path)
 
         number_of_candidates.append(filtered_bed_dat.shape[0])
         # Check if the causal splice junction is in the filtered data.
@@ -295,6 +410,8 @@ def main():
 
 
 if __name__ == "__main__":
+    # bigwig_path = "gs://tgg-rnaseq/batch_0/bigWig/210DB_BW_M1.bigWig"
+    # read_coverage_bigwig(bigwig_path, "chr19", 38433691, 38595273)
     main()
 #     dat1 = pd.DataFrame([["chr1", 1, 2, 5, "A"], ["chr1", 3, 4, 5, "A"], ["chr1",
 # 3, 10, 5, "B"]])
@@ -310,20 +427,20 @@ if __name__ == "__main__":
 #     print(dat2_normalized)
 #     get_sj_different_between_dats(dat1_normalized, dat2_normalized, 0.1)
 
-    # dat2 = pd.DataFrame([["chr1", 1, 2, 3], ["chr1", 3, 5, 3], ["chr2", 5, 7, 3]])
-    # dat2.columns = ["chr", "start", "end", "count"]
-    # print(get_sj_only_in_first_dat(dat1, dat2))
-    # gtex_dat = read_junctions_bed(GTEx_normalized, USE_COLS)
-    # gtex_dat = filter_by_uniquely_mapped_reads(gtex_dat, 1)
-    #
-    # junctions_bed_path = "gs://tgg-rnaseq/batch_0/junctions_bed_for_igv_js/41M_MW_M1.junctions.bed.gz"
-    # bed_dat = read_junctions_bed(junctions_bed_path, USE_COLS)
-    #
-    # # new_dat = get_sj_only_in_first_dat(gtex_dat, bed_dat)
-    # new_dat = compare_with_gtex(gtex_dat, bed_dat)
-    #
-    # pd.set_option('display.max_columns', None)
-    # print(new_dat)
-    # print(new_dat.columns)
-    # print(gtex_dat.shape)
-    # print(bed_dat.shape)
+# dat2 = pd.DataFrame([["chr1", 1, 2, 3], ["chr1", 3, 5, 3], ["chr2", 5, 7, 3]])
+# dat2.columns = ["chr", "start", "end", "count"]
+# print(get_sj_only_in_first_dat(dat1, dat2))
+# gtex_dat = read_junctions_bed(GTEx_normalized, USE_COLS)
+# gtex_dat = filter_by_uniquely_mapped_reads(gtex_dat, 1)
+#
+# junctions_bed_path = "gs://tgg-rnaseq/batch_0/junctions_bed_for_igv_js/41M_MW_M1.junctions.bed.gz"
+# bed_dat = read_junctions_bed(junctions_bed_path, USE_COLS)
+#
+# # new_dat = get_sj_only_in_first_dat(gtex_dat, bed_dat)
+# new_dat = compare_with_gtex(gtex_dat, bed_dat)
+#
+# pd.set_option('display.max_columns', None)
+# print(new_dat)
+# print(new_dat.columns)
+# print(gtex_dat.shape)
+# print(bed_dat.shape)
